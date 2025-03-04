6c39bb6906e24eb62ddab6198f39ed0768ed97b9
Michael Holland <mike1holland@hotmail.co.uk>
2025-03-04 18:24:06 +0000

    feat: add main logic for cost calculate

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,23 @@
 import { Command } from "commander";
 import { password } from "promptly";
 import { setAPIKey } from "./config/password";
+import {
+  calculateHouseHeatLoss,
+  calculatePowerHeatLoss,
+  getRecommendedHeatPump,
+  calculateCost,
+  getCostResult,
+} from "./models/cost";
+import houses from "./data/houses.json";
+import heatpumps from "./data/heat-pumps.json";
+import { getWeatherClient } from "./models/weather";
 
 const program = new Command();
 
 program
   .name("eecalc")
   .version("1.0.0")
-  .description("Evergreen Earth location efficiency calculator");
+  .description("Evergreen Earth cost calculator");
 
 program
   .command("set-api-key")
@@ -16,6 +26,50 @@ program
   .action(async () => {
     const apiKey = await password("Enter your Weather Data API key: ");
     await setAPIKey(apiKey);
+  });
+
+program
+  .command("calculate")
+  .description("Calculate the cost of installation based on house data")
+  .argument(
+    "submission-id",
+    "The submission ID of the house to calculate the cost for"
+  )
+  .action(async (submissionId) => {
+    const weatherClient = await getWeatherClient();
+    const house = houses.find((house) => house.submissionId === submissionId);
+    if (!house) {
+      console.error("House not found");
+      return;
+    }
+    const heatLoss = calculateHouseHeatLoss(house);
+    const powerHeatLoss = await calculatePowerHeatLoss(
+      house,
+      heatLoss,
+      weatherClient
+    );
+    if (!powerHeatLoss) {
+      console.error("Failed to calculate power heat loss");
+      return;
+    }
+    const recommendedHeatPump = await getRecommendedHeatPump(
+      heatpumps,
+      powerHeatLoss
+    );
+    if (!recommendedHeatPump) {
+      console.error("No recommended heat pump found");
+      return;
+    }
+    const totalCost = calculateCost(recommendedHeatPump);
+    const costResult = getCostResult(
+      house,
+      heatLoss,
+      powerHeatLoss,
+      recommendedHeatPump,
+      totalCost
+    );
+
+    console.log(costResult);
   });
 
 program.parse(process.argv);
